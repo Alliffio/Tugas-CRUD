625e317035f9df25fd6f9e7c116e8bd5a16263ba
Alliffio <55769866+Alliffio@users.noreply.github.com>
2020-06-05 08:47:12 +0700

    Add files via upload

---
+++ crud-ajax/koneksi.php
@@ -0,0 +1,10 @@
+<?php
+
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$db = 'db_starbhak';
+
+$connection = mysqli_connect( $host, $user, $password, $db ) or die( mysqli_error() );
+
+?>
